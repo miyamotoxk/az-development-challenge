6331ac3826ee85262d73fbea06be666337fd9efd
Arden ZHONG <zhong.arden@gmail.com>
2020-11-16 04:18:05 +1000

    Built a simple app with express and ejs.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-console.log('-----');
-console.log('Viveo Group Developer Challenge');
-console.log('Replace ./src/index.ts to get started!');
-console.log('-----');
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import * as routes from "./routes";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.SERVER_PORT || 3000;
+
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Configure routes
+routes.register(app);
+
+// start the Express server
+app.listen(port, () => {
+  // tslint:disable-next-line: no-console
+  console.log(`server started at http://localhost:${port}`);
+});
---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import * as express from "express";
+
+export const register = (app: express.Application) => {
+  // define a route handler for the default home page
+  app.get("/", (req: any, res) => {
+    res.render("index");
+  });
+
+  // define a route handler for the calculator page
+  app.get("/calculator", (req: any, res) => {
+    res.render("calculator");
+  });
+};
